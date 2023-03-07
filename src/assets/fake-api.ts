5a535a800cda6166525d12b65ef8f381316a2e33
ahmed-elrawy <aelrawy29@gmail.com>
2023-03-07 22:44:39 +0200

    edite fake api

--- src/assets/fake-api.ts
+++ src/assets/fake-api.ts
@@ -6,7 +6,7 @@ export const PRODUCTS_DATA: Product[] = [
     {id: 3, title: "t-shirt", price: 134, description: "desc", category: "fashion", image: "https://placehold.jp/100x100.png", rating: {rate:4, count: 4 }},
     {id: 4, title: "PC", price: 777, description: "desc", category: "electronic", image: "https://placehold.jp/100x100.png", rating: {rate:1, count: 77 }},
     {id: 5, title: "PS 7", price: 1, description: "desc", category: "gaming", image: "https://placehold.jp/100x100.png", rating: {rate:2, count: 78 }},
-    {id: 6, title: "title", price: 3456, description: "desc", category: "electronic", image: "./imhttps://placehold.jp/100x100.png", rating: {rate:4, count: 1 }},
+    {id: 6, title: "title", price: 3456, description: "desc", category: "electronic", image: "https://placehold.jp/100x100.png", rating: {rate:4, count: 1 }},
     {id: 7, title: "PS 5", price: 7000, description: "desc", category: "gaming", image: "https://placehold.jp/100x100.png", rating: {rate:4, count: 1 }},
     {id: 8, title: "TV samsung", price: 1000, description: "desc", category: "electronic", image: "https://placehold.jp/100x100.png", rating: {rate:3, count: 44 }},
     {id: 9, title: "IOS", price: 133, description: "desc", category: "gaming", image: "https://placehold.jp/100x100.png", rating: {rate:1, count: 1 }},
